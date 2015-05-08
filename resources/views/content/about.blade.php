edafd2233039ada17df1fd69616d2d8f076a1f89
Osamu Fujimoto <oaf7862@rit.edu>
2015-05-07 23:13:28 -0400

    updated about

--- resources/views/content/about.blade.php
+++ resources/views/content/about.blade.php
@@ -5,6 +5,7 @@
 <div class="container">
 <h2> About DB : The MMO </h2>
 
+<img src = "{{ asset('cache/logo.png') }}" style="display: block; margin-left: auto; margin-right: auto;"></img>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -13,26 +14,6 @@ cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </s>
 </div>
-
-<script>
-$(".example7").colorbox({iframe:true, innerWidth:425, innerHeight:344});
-</script>
-
-
-<div class="bs-example">
-    <!-- Button HTML (to Trigger Modal) -->
-    <a href="about" class="btn btn-lg btn-primary" data-toggle="modal" data-target="#myModal">Launch Demo Modal</a>
-    
-    <!-- Modal HTML -->
-    <div id="myModal" class="modal fade">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <!-- Content will be loaded here from "remote.php" file -->
-            </div>
-        </div>
-    </div>
-</div>
-
 
     <!-- 1jQuery Version 1.11.1 -->
     <script src="{{ asset('js/jquery.js') }}"></script>
